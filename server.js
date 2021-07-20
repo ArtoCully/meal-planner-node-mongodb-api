00507b35c4cdd02b0bb68d72dd401bedde0a4a21
Arturo Cullinane <artocully@users.noreply.github.com>
2021-07-20 10:09:53 +0100

    Add initial dotenv values in .env.example

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 require('rootpath')();
 const express = require('express');
 const app = express();
@@ -21,7 +23,8 @@ app.use('/api/recipes', require('./recipes/recipes.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3001;
+console.log('process', process.env);
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (process.env.PORT || 3001);
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
